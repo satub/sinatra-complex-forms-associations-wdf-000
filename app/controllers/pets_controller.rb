a54532dc08bf0b2d85522f16eb7a8e93dcfc0479
satub <satu.barnhill@gmail.com>
2016-08-16 08:50:15 -0400

    Done with checkboxes

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -2,25 +2,58 @@ class PetsController < ApplicationController
 
   get '/pets' do
     @pets = Pet.all
-    erb :'/pets/index' 
+    erb :'/pets/index'
   end
 
-  get '/pets/new' do 
+  get '/pets/new' do
     erb :'/pets/new'
   end
 
-  post '/pets' do 
-
+  post '/pets' do
+    @pet = Pet.create(params[:pet])
+    if !params[:pet][:owner_id].nil?
+      owners = Owner.find(params[:pet][:owner_id])
+      owners.collect do |owner|
+        owner.pets << @pet
+        owner.save
+      end
+    end
+    if !params[:owner][:name].empty?
+      owner = Owner.create(name: params[:owner][:name])
+      owner.pets << @pet
+      owner.save
+    end
+    @pet.save
     redirect to "pets/#{@pet.id}"
   end
 
-  get '/pets/:id' do 
+  get '/pets/:id/edit' do
     @pet = Pet.find(params[:id])
+    erb :'/pets/edit'
+  end
+
+  get '/pets/:id' do
+    @pet = Pet.find(params[:id])
+    @owner = Owner.find(@pet.owner_id)
     erb :'/pets/show'
   end
 
-  post '/pets/:id' do 
-
+  post '/pets/:id' do
+    @pet = Pet.find(params[:id])
+    @pet.update(params["pet"])
+    if !params[:pet][:owner_id].nil?
+      owners = Owner.find(params[:pet][:owner_id])
+      owners.collect do |owner|
+        owner.pets << @pet
+        owner.save
+      end
+    end
+    if !params[:owner][:name].empty?
+      owner = Owner.create(name: params[:owner][:name])
+      owner.pets << @pet
+      owner.save
+    end
+    @pet.save
     redirect to "pets/#{@pet.id}"
   end
 end
